0576665fcfa80dd5641cb55674326d0e2dfe1766
LiisaVuorenmaa <liisa.kuisma@hotmail.fi>
2021-03-09 22:14:32 +0200

    ultrasensor korjattu run metodi

--- UudetKoodit/src/Data/UltraSensor.java
+++ UudetKoodit/src/Data/UltraSensor.java
@@ -5,8 +5,7 @@ package Data;
 import lejos.hardware.Sound;
 import lejos.hardware.port.SensorPort;
 import lejos.hardware.sensor.EV3UltrasonicSensor;
-
- 
+import util.DATA;
 
 import java.io.File;
 
@@ -16,23 +15,24 @@ public class UltraSensor implements Runnable{
 
  
 
-        @Override
-        public void run() {
+        
 
  
 
             UltraSonicSensor uss = new UltraSonicSensor(SensorPort.S2);
 
  
-
-            
+           
+        	public void run() {
             while(Data.shouldRun)
             {
                 Data.range = uss.getRange();
                 System.out.println("Ultrasensor going");
                 System.out.println(Data.range);
-            
+                
             }
+            
+            
             
     
         }
