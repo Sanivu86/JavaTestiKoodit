aa3c210ea28b1eb8231c147bd5b725e7e7ea59cb
Sanivu86 <59826845+Sanivu86@users.noreply.github.com>
2021-03-12 19:45:38 +0200

    Data-luokka kommentoitu

--- LejosEv3Project/src/Data/Data.java
+++ LejosEv3Project/src/Data/Data.java
@@ -1,17 +1,42 @@
 package Data;
 
-import lejos.robotics.Color;
-
+/**
+ * 
+ * @author Jenni Lehtonen, Liisa Vuorenmaa, Alina Rouvinen, Sanna Nieminen-Vuorio
+ * 
+ * Anturit tallentavat tiedot Data-luokan staattisiin muuttujiin
+ *
+ */
 public class Data {
 	
-	public static boolean shouldRun = true;  //K‰ytet‰‰n while-looppien ehtona
-	public static boolean isTouch = false;  //Touch-sensorin ehto
-	public static float range = 100; //Tiedot tulevat UltraSensor-luokasta
-	public static float colorline = 45;  //V‰risensorilta tuleva v‰riarvo, joka on kerrottu 1000:lla
-	public static int currentColor = 7;  //Tiedot tulevat ColorSensor-luokasta, v‰risensorin hakema v‰riID, 0=punainen
-	public static int counter = 0; //Laskee esteet
+	/**
+	 * K‰ytet‰‰n while-looppien ehtona
+	 */
+	public static boolean shouldRun = true;  
+	/**
+	 * Touch-sensorin ehto. Asetetaan trueksi, jos anturi aktivoituu
+	 */
+	public static boolean isTouch = false;  
+	/**
+	 * UltraSensor-luokassa ultrasensori mittaa et‰isyyden ja tallettaa sen t‰h‰n
+	 */
+	public static float range = 100; 
+	/**
+	 * V‰risensorilta tuleva v‰riarvo, joka on kerrottu 1000:lla
+	 */
+	public static float colorline = 45;  
+	/**
+	 * Tiedot tulevat ColorSensor-luokasta, v‰risensorin hakema v‰riID, 0=punainen
+	 */
+	public static int currentColor = 7;  
+	/**
+	 * Lis‰t‰‰n lukemaa, kun robotti havaitsee esteen
+	 */
+	public static int counter = 0; 
 	
-	//Robotin renkaiden nopeudet eri tilanteissa:
+	/**
+	 * Robotin renkaiden nopeudet eri tilanteissa:
+	 */
 	public static final int AVARAGEVELOCITY = 30;
 	public static final int SLOWDOWN = 20;
 	public static final int STOPDRIVING = 0;
