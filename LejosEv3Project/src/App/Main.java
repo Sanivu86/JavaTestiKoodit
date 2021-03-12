0a98d1bd2e4d021b28f837cf69bd195c629b937e
LiisaVuorenmaa <liisa.kuisma@hotmail.fi>
2021-03-12 22:43:12 +0200

    Kommentoitu Touch-luokka

--- LejosEv3Project/src/App/Main.java
+++ LejosEv3Project/src/App/Main.java
@@ -12,7 +12,7 @@ public class Main {
 
 	
 /**
- * @author liisa
+ * @author liisa, Sanna, Jenni, Alina
  */
 
 	/**
--- LejosEv3Project/src/Data/Touch.java
+++ LejosEv3Project/src/Data/Touch.java
@@ -5,7 +5,13 @@ import lejos.hardware.sensor.EV3ColorSensor;
 import lejos.hardware.sensor.EV3TouchSensor;
 import lejos.hardware.sensor.SensorMode;
 import lejos.robotics.SampleProvider;
-
+/**
+ * @author liisa
+ * T‰m‰ luokka komentaa Touch- sensoria, luokka k‰ytt‰‰ runnable rajapintaa ja sis‰lt‰‰ run- metodin ja isTouched-metodin
+ * Touch- sensori saa arvot Data-luokan stattisesta muuttujasta isTouch
+ * Touch- sensoriin liittyv‰ toiminto asetetaan moottoriluokasta
+ * Touch- sensoria k‰ytet‰‰n s‰ikeen‰
+ */
 public class Touch implements Runnable {
 
 	EV3TouchSensor sensor;
@@ -15,20 +21,29 @@ public class Touch implements Runnable {
 	@Override
 	public void run() {
 		// TODO Auto-generated method stub
-	
+	/**
+	 * Luodaan Touch- sensori
+	 */
 		sensor = new EV3TouchSensor(SensorPort.S3);
 		sp = sensor.getTouchMode();
+		/**
+		 * While- Looppi touch- sensorin k‰ynniss‰ pit‰miseen, jossa k‰ytet‰‰n ehtona Data-luokan shouldRun muuttujaa ja kutsutaan isTouched- metodia.
+		 */
 		while(Data.shouldRun)
 		{
 			IsTouched();
 			System.out.println("Touchsensor going");
 		    
 		}
-		
+		/**
+		 * Suljetaan touch-sensori
+		 */
 		sensor.close();
 		
 	}
-	
+		/**
+		 * IsTouched- metodi jossa verrataan Touch-sensorin saamia arvoja
+		 */
 	 public void IsTouched()
 	    {
 	       float [] sample = new float[sp.sampleSize()];
